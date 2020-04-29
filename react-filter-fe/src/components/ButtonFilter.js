c1269f55b5ee8950c0c74f86b3fafedb7c41f6a5
morenoloza98 <morenolozalex98@gmail.com>
2020-04-29 00:08:16 -0500

    Initial commit

---
+++ react-filter-fe/src/components/ButtonFilter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../css/ButtonFilter.css';
+
+function ButtonFilter(){
+    return(
+        <div>
+            <h3>Titulo</h3>
+        </div>
+    )
+}
+
+export default ButtonFilter;
